4f6da68e83109e9e9f1595b89163f6fb555ab348
tingyuan <tingyuan@iflytek.com>
2021-12-03 16:55:59 +0800

    feat:push variable

--- lib/dart/valueable/test.dart
+++ lib/dart/valueable/test.dart
@@ -1,3 +1,17 @@
-void main(){
+void main() {
+  /// 1、dart变量仅存储对象的引用
+  var name = "Jocelyn";
+  /// 2、延迟初始化
+  late String description;
 
+  /// 3、final const
+  /// 不可变
+  final newName = "Jocelyn";
+  // 常量
+  const constVaries = "Jocelyn";
+
+  /// 默认值为null,空安全？
+  /// 在使用空安全的关键字时，需要将项目所有package迁移至空安全
+  /// 迁移方法 https://dart.dev/null-safety/migration-guide
+  int? lineCount = null;
 }
