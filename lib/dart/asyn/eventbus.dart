75fe87e6cd9f1381e8bb7600cad3382ef6b21809
tingyuan <tingyuan@iflytek.com>
2021-12-21 20:50:43 +0800

    feat: 实现Dart eventbus

---
+++ lib/dart/asyn/eventbus.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+class EventBus {
+  static late EventBus _instance;
+  late StreamController _streamController;
+
+  factory EventBus.getDefault() {
+    return _instance;
+  }
+
+  EventBus._init() {
+    _streamController = StreamController.broadcast();
+  }
+
+  StreamSubscription<T>? register<T>(void onData(T event)?) {
+    Stream<T> stream;
+    if (T == dynamic) {
+      stream = _streamController.stream as Stream<T>;
+    } else {
+      stream =  _streamController.stream.where((type) => type is T).cast<T>();
+      return stream.listen(onData);
+    }
+  }
+
+  void post(event) {
+    _streamController.add(event);
+  }
+
+  void unregister() {
+    _streamController.close();
+  }
+}
+void main(){
+
+}
